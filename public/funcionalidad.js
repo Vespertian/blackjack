14b03ef0855bb4416c80ad5913b1177f1c401b69
M4rteaga <mateoarteagagiraldo@gmail.com>
2020-11-01 20:25:10 -0500

    intento conectar

--- public/funcionalidad.js
+++ public/funcionalidad.js
@@ -3,18 +3,35 @@ var app = new Vue({
     data: {
         inicio: true,
         jugar: false,
-        suma: 21,
-        apuesta: 2000
+        suma: 0,
+        apuesta: 0,
+        ganancia: 0,
+        datosJuego: ''
     },
     methods: {
-        comenzarAJugar() {
+        async comenzarAJugar() {
+
             this.inicio = false;
             this.jugar = true;
+
+            this.datosJuego = await fetch('http://172.105.20.118:8080/join')
+
+            console.log(err);
+
         },
         salirseDelJuego() {
             this.inicio = true;
             this.jugar = false;
-        }
+        },
+        apostar1000() {
+            this.apuesta += 1000;
+        },
+        apostar2000() {
+            this.apuesta += 2000;
+        },
+        apostar5000() {
+            this.apuesta += 5000;
+        },
     }
 
 });
